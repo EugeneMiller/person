dd87dac3d13777ad7fc3becb1ba66370b0fde17d
Евгений Мельник <good.production@rambler.ru>
2022-04-18 22:53:43 +0500

    hw2

--- src/app/exp-items.ts
+++ src/app/exp-items.ts
@@ -1,8 +1,8 @@
 import{job} from './job';
 export const ITEMS : job[] = [
 {position:'front', desc:'front-desc', startDate: new Date("2017-05-24"), endDate: new Date("2017-05-24")},
-{position:'back', desc:'back-desc', startDate: new Date("2017-05-24"), endDate: new Date("2017-05-24")},
-{position:'full', desc:'full-desc', startDate: new Date("2017-05-24"), endDate: new Date("2017-05-24")},
-{position:'full', desc:'full-desc', startDate: new Date("2017-05-24"), endDate: new Date("2017-05-24")},
-{position:'full', desc:'full-desc', startDate: new Date("2017-05-24"), endDate: new Date("2017-05-24")}
+{position:'back', desc:'back-desc', startDate: new Date("2018-05-24"), endDate: new Date("2017-07-24")},
+{position:'full', desc:'full-desc', startDate: new Date("2019-05-24"), endDate: new Date("2019-11-24")},
+{position:'full', desc:'full-desc', startDate: new Date("2021-05-24"), endDate: new Date("2021-11-24")},
+{position:'full', desc:'full-desc', startDate: new Date("2021-11-24"), endDate: new Date("2022-02-24")}
 ];
--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { registerLocaleData } from '@angular/common';
+import{job} from '../job'; // импортирую тип данных
 import localeRu from '@angular/common/locales/ru';
 registerLocaleData(localeRu, 'ru');
 
@@ -9,12 +10,27 @@ registerLocaleData(localeRu, 'ru');
   styleUrls: ['./experience.component.css']
 })
 export class ExperienceComponent implements OnInit {
+  @Input() eItem: job = {
+    position: '',
+    desc: '',
+    startDate: new Date(),
+  endDate:new Date()};
   position: string = 'front-end developer';
   desc: string = 'Программирование на HTML+CSS+JS';
-  startDate: Date  = new Date("2017-05-24");
-  endDate: Date = new Date("2018-07-29");
+  startDate: Date  = new Date("2021-05-24");
+  endDate: Date = new Date("2021-07-29");
+  isHide : Boolean = false;
+  nowDay: Date = new Date();
   constructor() { }
+  //показать байндинг
+  firmURL = 'https://img.huffingtonpost.com/asset/56e6e7651e000087007040c6.jpeg';
+hideDesc=() =>{
+  this.isHide = !this.isHide;
+}
+pastDays(d : Date) : number{
 
+  return (Date.now()-d.getTime())/60/60/24/1000;
+}
   ngOnInit(): void {
   }
 
